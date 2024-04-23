1a0a65c08912fecba2c2ba59534fea81151748ac
Francesco Cristiano <frankrys97@gmail.com>
2024-04-23 16:45:57 +0200

    adds search

--- src/components/AllBooks2.jsx
+++ src/components/AllBooks2.jsx
@@ -11,54 +11,69 @@ import React from "react";
 class AllBooks2 extends React.Component {
   state = {
     selectedJson: fantasy,
+    searchInput: "",
   };
   render() {
+    const searchedBook = this.state.selectedJson.filter((book) => {
+      return book.title
+        .toLowerCase()
+        .includes(this.state.searchInput.toLowerCase());
+    });
     return (
       <Container className="mt-3">
-        <ButtonGroup aria-label="Basic example" className="my-3">
-          <Button
-            variant="outline-secondary"
-            onClick={(e) => {
-              this.setState({ selectedJson: fantasy });
-            }}
-          >
-            Fantasy
-          </Button>
-          <Button
-            variant="outline-secondary"
-            onClick={(e) => {
-              this.setState({ selectedJson: history });
-            }}
-          >
-            History
-          </Button>
-          <Button
-            variant="outline-secondary"
-            onClick={(e) => {
-              this.setState({ selectedJson: horror });
-            }}
-          >
-            Horror
-          </Button>
-          <Button
-            variant="outline-secondary"
-            onClick={(e) => {
-              this.setState({ selectedJson: romance });
-            }}
-          >
-            Romance
-          </Button>
-          <Button
-            variant="outline-secondary"
-            onClick={(e) => {
-              this.setState({ selectedJson: scifi });
-            }}
-          >
-            Scifi
-          </Button>
-        </ButtonGroup>
+        <div className="d-flex justify-content-between align-items-center">
+          <ButtonGroup aria-label="Basic example" className="my-3">
+            <Button
+              variant="outline-secondary"
+              onClick={(e) => {
+                this.setState({ selectedJson: fantasy });
+              }}
+            >
+              Fantasy
+            </Button>
+            <Button
+              variant="outline-secondary"
+              onClick={(e) => {
+                this.setState({ selectedJson: history });
+              }}
+            >
+              History
+            </Button>
+            <Button
+              variant="outline-secondary"
+              onClick={(e) => {
+                this.setState({ selectedJson: horror });
+              }}
+            >
+              Horror
+            </Button>
+            <Button
+              variant="outline-secondary"
+              onClick={(e) => {
+                this.setState({ selectedJson: romance });
+              }}
+            >
+              Romance
+            </Button>
+            <Button
+              variant="outline-secondary"
+              onClick={(e) => {
+                this.setState({ selectedJson: scifi });
+              }}
+            >
+              Scifi
+            </Button>
+          </ButtonGroup>
+          <input
+            type="text"
+            placeholder="Search..."
+            value={this.state.searchInput}
+            onChange={(e) => this.setState({ searchInput: e.target.value })}
+            className="rounded rounded-2 p-1 border border-1 border-dark"
+          />
+        </div>
         <Row xs={1} sm={2} md={3} lg={4} className="g-4">
-          {this.state.selectedJson.map((book) => (
+          {searchedBook.map((book) => (
             <Col>
               <Card key={`book ${book.asin}`} className="h-100">
                 <Card.Img
